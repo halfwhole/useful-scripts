b998d5b78e8d9bc6683c6b8162a73e49cd4eb5f1
halfwhole <limziwei2@gmail.com>
2019-12-24 18:01:26 +0800

    Allow multiple images/videos for Instagram downloads

--- instagram-download.js
+++ instagram-download.js
@@ -1,10 +1,22 @@
 javascript:(
     function() {
-        const pics = document.querySelector("article").querySelectorAll("img");
-        const non_profile_pics = [...pics].filter(x => x.getAttribute("decoding") !== null);
-        const non_profile_pics_urls = non_profile_pics.map(x => x.getAttribute("src"));
-        /* Only takes the first one for now */
-        const url = non_profile_pics_urls[0];
-        window.location.href = url;
+        /* Converts an image or video node to HTML */
+        function htmlize_image_video_node(node) {
+            return node.is_video
+                ? "<video controls><source src=" + node.video_url + "></video>"
+                : "<img src=" + node.display_url + ">";
+        }
+
+        /* Converts a sidecar node to HTML */
+        function htmlize_sidecar_node(node) {
+            const child_nodes = node.edge_sidecar_to_children.edges.map(x => x.node);
+            const html_images_videos = child_nodes.map(node => htmlize_image_video_node(node));
+            return html_images_videos.join("\n");
+        }
+
+        const parent = window._sharedData.entry_data.PostPage[0].graphql.shortcode_media;
+        document.body.innerHTML = parent.__typename === "GraphSidecar"
+            ? htmlize_sidecar_node(parent)
+            : htmlize_image_video_node(parent);
     }
 )();
